d4d35bd12a9c3084b83f6e353963636b332daede
Roman Onosovski <romano@arista.com>
2022-12-13 18:54:18 -0800

    Revert "Remove f-string from logger"
    This reverts commit 3b637c25daa34e8a2077cc0f48cd283ddac4c9f2.

--- examples/test_showver.py
+++ examples/test_showver.py
@@ -4,7 +4,7 @@ import pytest
 # A simplified demo
 def test_showver(dut):
     info = dut.eapi.sendcmd("show version")
-    logging.info("DUT model was: {}".format(info['modelName']))
+    logging.info(f"DUT model was: {info['modelName']}")
 
 
 # A demo of an assertion, which we expect to fail
